4b790b1fe794971e30761b5f7b4b471eb465b4e7
Siluxsept <ronny.horn.rh@gmail.com>
2026-01-24 12:25:52 +0100

    redbull: AICPify
    Change-Id: I2aa4f4b5d4efe8b4af951319ad3a81e2e0683be2

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -29,7 +29,7 @@ AB_OTA_PARTITIONS += \
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Reserve space for gapps install
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/aicp/config/BoardConfigReservedSize.mk
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-lineage/dynamic
